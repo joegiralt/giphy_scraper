7a94bddbd0467cbf06961a9f7445d332eaec96e6
Joseph Giralt <josephgiralt@Josephs-MacBook-Pro.local>
2021-02-04 16:12:05 -0500

    first commit

---
+++ lib/giphy_scraper.ex
@@ -0,0 +1,18 @@
+defmodule GiphyScraper do
+  @moduledoc """
+  Documentation for `GiphyScraper`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GiphyScraper.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/giphy_scraper_test.exs
@@ -0,0 +1,8 @@
+defmodule GiphyScraperTest do
+  use ExUnit.Case
+  doctest GiphyScraper
+
+  test "greets the world" do
+    assert GiphyScraper.hello() == :world
+  end
+end
